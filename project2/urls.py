7732381b44a1d097adeecc892b69589194abad50
Riyasha lad <riyashalad1421@gmail.com>
2025-07-04 12:59:41 +0200

    Merge branch 'main' of https://github.com/Riyashalad1421/HCAI_PROJECT

--- project2/urls.py
+++ project2/urls.py
@@ -5,4 +5,7 @@ app_name = 'project2'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('active/', views.active_learning_view, name='active_learning'),
+    path('active/loop/', views.active_learning_loop, name='active_learning_loop'),
+    path('active/reset/', views.reset_active_session, name='active_reset'),
 ]
