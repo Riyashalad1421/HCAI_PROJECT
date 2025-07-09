7135d63839a70ad4ef44730dabfbed4f31b014e4
Riyasha lad <riyashalad1421@gmail.com>
2025-07-10 00:02:10 +0200

    porject 4

--- project4/views.py
+++ project4/views.py
@@ -1,10 +1,11 @@
+from .utils import simulate_rating_impact
 from django.shortcuts import render
 from django.http import HttpResponse
 import io
 from reportlab.pdfgen import canvas
 from pathlib import Path
 import pandas as pd
-#from .utils import simulate_rating_impact
+from .models import Feedback 
 
 
 def project4_landing(request):
@@ -15,21 +16,43 @@ def project4_landing(request):
     return render(request, 'project4/landing.html')
 
 
+from .models import Feedback
+from .utils import simulate_rating_impact
+from django.shortcuts import render
+from pathlib import Path
+import pandas as pd
+
 def project4_study(request):
-    """
-    Simulated user interaction for Project 4 Task 3.
-    """
     BASE_DIR = Path(__file__).resolve().parent
     movies_path = BASE_DIR / "data" / "movies.csv"
     movies_df = pd.read_csv(movies_path)
     movie_title_to_id = dict(zip(movies_df['title'], movies_df['movieId']))
+    all_titles = sorted(movie_title_to_id.keys())
 
-    selected_movie = "Inception"
-    prediction_impact = simulate_rating_impact(selected_movie, movie_title_to_id)
+    selected_movie = None
+    prediction_impact = None
+    feedback_submitted = False
+
+    if request.method == "POST":
+        selected_movie = request.POST.get("movie_title", "")
+        if 'submit_feedback' in request.POST:
+            # Save feedback
+            Feedback.objects.create(
+                movie_title=selected_movie if selected_movie else "Unknown",
+                helpfulness=request.POST.get("feedback", "None"),
+                comments=request.POST.get("comments", "")
+            )
+            feedback_submitted = True
+
+        # Only calculate impact if a movie is selected
+        if selected_movie:
+            prediction_impact = simulate_rating_impact(selected_movie, movie_title_to_id)
 
     return render(request, 'project4/study.html', {
         'movie_title': selected_movie,
-        'prediction_impact': prediction_impact
+        'prediction_impact': prediction_impact,
+        'all_titles': all_titles,
+        'feedback_submitted': feedback_submitted
     })
 
 def project4_download_pdf(request):
